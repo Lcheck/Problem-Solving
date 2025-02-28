4fe8698ff04cb4e5cc29d6bfa138e8edd0f77797
Lcehck <znfhdl789@naver.com>
2025-02-28 17:54:56 +0900

    leet_TwoSum : 해시맵활용

--- LeetCode/TwoSum.java
+++ LeetCode/TwoSum.java
@@ -1,6 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
 class Solution {
     int varTarget;
     int[] result = new int[2];
+
 	public int[] twoSum(int[] nums, int target) {
 
 		//1.주어진 배열로 브루트포스
@@ -21,6 +26,34 @@ class Solution {
 				}
 				
 		}
+
+        return result;
+	}
+
+}
+
+class Solution2 {
+ 
+	int[] result = new int[2];
+	Map<Integer, Integer> map = new HashMap<>();
+	public int[] twoSum(int[] nums, int target) {
+
+		//2.해시맵 활용
+		// -> 현재 인덱스, 값매핑 하여 "기록" 후 다음 순회에 활용
+        
+			for (int i = 0; i < nums.length; i++) 
+			{	
+
+				if(map.containsKey(target-nums[i]))
+				{
+					
+					result[0]=map.get((target-nums[i]));
+                    result[1]=i;
+				}
+
+                map.put(nums[i],i);
+                //put이 앞에있으면 중복값있는 경우 현재 값의 인덱스가 출력됨
+			}
 
         return result;
 	}
